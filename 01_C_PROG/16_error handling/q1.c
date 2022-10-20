17309a4774059129a53f212879d79be68555453a
anuj-raj <eee.19610304@silicon.ac.in>
2022-10-20 13:21:27 +0530

    first commit

---
+++ 01_C_PROG/16_error handling/q1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+  
+int main ()
+{
+    FILE *fp;
+    fp = fopen("hello.txt ", "r");
+    printf("Value of errno: %d\n ", errno);
+    printf("The error message is : %s\n", 
+                         strerror(errno));
+    perror("Message from perror");
+  
+    return 0;
+}
